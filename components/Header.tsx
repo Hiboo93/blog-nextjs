7c7eb0a8e33185f6b9f2e097f634a62deabe0c29
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-03-12 20:15:04 +0100

    ✨ F02: creation of the component ResponsiveMenu and installing the sheet component

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PageContainer from './Page-container'
 import { HeaderNavigation } from './Header-navigation'
 import ProfileButton from './profile-button'
+import ResponsiveMenu from './ResponsiveMenu'
 
 
 type Props = {}
@@ -11,8 +12,9 @@ const Header = (props: Props) => {
     <header className='p-4 border-b'>
       <PageContainer>
         <div className="flex items-center justify-between w-full">
-          <div>
+          <div className='flex items-center gap-2'>
             {/* responsive menu */}
+            <ResponsiveMenu/>
             <h1 className='text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-fuchsia-400 to-blue-600'>NextBlog</h1>
           </div>
           
