fe2e4d4da6d3222d75065f993ecbc3d1fcb334ff
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-03-12 18:48:10 +0100

    ✨ F01: creation of the component Header-navigation and ProfileButton

---
+++ components/Profile-button.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link.js'
+import React from 'react'
+import { Button } from './ui/button'
+
+type Props = {}
+
+const ProfileButton = (props: Props) => {
+
+  return (
+    <Link href="/login">
+      <Button>
+        Login
+      </Button>
+    </Link>
+  )
+}
+
+export default ProfileButton
