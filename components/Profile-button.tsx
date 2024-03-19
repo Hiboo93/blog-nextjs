b8a7cc43c7cd447893bc5e69e85ebb9651e64f6c
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-03-19 10:09:39 +0100

    🚧 F05: work in progress in section one and add backgroungImage

--- components/Profile-button.tsx
+++ components/Profile-button.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link.js';
+import Link from 'next/link';
 import React from 'react';
 import { Button } from './ui/button';
 
--- components/toggleTheme.tsx
+++ components/toggleTheme.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Moon, Sun } from 'lucide-react'
 import { Button } from './ui/button'
