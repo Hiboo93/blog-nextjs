8a1c03527799454187a5988ed27e68b1074e27fa
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-03-20 19:24:47 +0100

    ✨ F06: creating postCard component

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,10 +1,77 @@
+import PageContainer from "@/components/Page-container";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Separator } from "@/components/ui/separator";
+import { Post } from "@/types";
+import { Eye, MessageCircle } from "lucide-react";
 
-type Props = {}
 
-function SinglePostsPage({}: Props) {
+function SinglePostsPage() {
+  const POST: Post = {
+    id: 1,
+    category: "React",
+    title: "React State Management: Choosing the Right Solution",
+    image: "/react-state-management.jpg",
+    caption:
+      "Explore different state management solutions in React and choose the one that fits your needs.",
+    date: "2023-01-15",
+    minutesToRead: 10,
+    author: "John ReactDev",
+    nbViews: 25,
+    nbComments: 8,
+    slug: "react-state-management-choosing-right-solution",
+    content: "Hello",
+  };
   return (
-    <div>SinglePostsPage</div>
-  )
+    <PageContainer>
+      <div className="p-8">
+        <div
+          className="rounded-lg aspect-square md:aspect-[2.4/1] overflow-hidden"
+          style={{ backgroundImage: "url(/img/hero.jpg)" }}
+        >
+          <div className="h-full w-full flex flex-col justify-center items-center">
+            <div className="sm:max-w-xl max-w-xs bg-secondary/80 p-4 rounded-lg">
+              <h1 className="text-center font-bold text-3xl sm:text-5xl text-black dark:text-white">
+                {POST.title}
+              </h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-between items-center p-3 mb-3">
+          <div className="flex justify-center items-center gap-3">
+            <Avatar>
+              <AvatarImage src="/img/xavatario.png" />
+              <AvatarFallback>{POST.author}</AvatarFallback>
+            </Avatar>
+            <div>
+              <p>{POST.author}</p>
+              <p className="text-slate-500 text-sm">
+                Posted on {new Date(POST.date).toLocaleDateString()}
+              </p>
+            </div>
+          </div>
+
+          <div className="flex gap-2">
+            <div className="flex items-center gap-1">
+              <MessageCircle size={24} />
+              <p>{POST.nbComments}</p>
+            </div>
+            <div className="flex items-center gap-1">
+              <Eye size={24} />
+              <p>{POST.nbViews}</p>
+            </div>
+          </div>
+        </div>
+        <Separator />
+
+        {/* section 2 */}
+        <div className="mt-6" dangerouslySetInnerHTML={{
+          __html: POST.content as string,
+        }}>
+        </div>
+      </div>
+    </PageContainer>
+  );
 }
 
-export default SinglePostsPage
+export default SinglePostsPage;
